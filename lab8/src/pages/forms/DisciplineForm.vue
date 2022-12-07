63dd0b8427f661dbfa2a43819c7183d2a923e830
Maxim <maxim.mishchuk@gmail.com>
2022-12-07 16:12:10 +0200

    DisciplineForm, StudentForm, TeacherFrom

--- lab8/src/pages/forms/DisciplineForm.vue
+++ lab8/src/pages/forms/DisciplineForm.vue
@@ -1,10 +1,25 @@
 <template>
-
+  <form action="" method="post" @submit.prevent>
+    <input type="text" placeholder="Name">
+    <input type="submit" value="Add" @click="process">
+  </form>
 </template>
 
 <script>
 export default {
-  name: "DisciplineForm"
+  name: "DisciplineForm",
+  data() {
+    return {
+      discipline: {
+        name: '',
+      }
+    }
+  },
+  methods: {
+    process() {
+      this.$emit("addDiscipline", this.discipline)
+    }
+  }
 }
 </script>
 
--- lab8/src/pages/forms/StudentForm.vue
+++ lab8/src/pages/forms/StudentForm.vue
@@ -1,10 +1,42 @@
 <template>
-
+  <form action="" method="post" @submit.prevent>
+    <input v-model="student.name" type="text" placeholder="Name">
+    <input v-model="student.email" type="email" name="" placeholder="Email">
+    <input v-model="student.phone" type="tel" name="" placeholder="Phone">
+    <select name="">
+      <option value="">Group</option>
+      <option v-for="group in groups" value="{{ group.id }}">
+        {{ group.name }}
+      </option>
+    </select>
+    <input type="submit" value="Add" @click="process">
+  </form>
 </template>
 
 <script>
 export default {
-  name: "StudentForm"
+  name: "StudentForm",
+  props: {
+    groups: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      student: {
+        group_id: '',
+        name: '',
+        email: '',
+        phone: ''
+      }
+    }
+  },
+  methods: {
+    process() {
+      this.$emit("addStudent", this.student)
+    }
+  }
 }
 </script>
 
